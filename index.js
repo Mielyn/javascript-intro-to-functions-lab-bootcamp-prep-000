b440695ee11852ae5184104437cfeef11b4dfdea
Mielyn Acosta <mielyndl@gmail.com>
2018-09-04 20:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-function speak(){
-  
-}
-
- function shout(String) {
-  return String.toUpperCase()
-  console.log(String);
- }
+var uppercase, lowercase
  
- function whisper(String) {
-    return String.toLowerCase()
-   console.log(String);
-  }
-  
-  function logShout(String){
-    console.log();
-  }
-  
-  function logWhisper(String) {
-    return String.toLowerCase()
-  }
-  
-  function sayHiToGrandma(String){
-    return String();
-  }
+function shout(string){
+  uppercase = string.toUpperCase();
+  string = uppercase;
+  return uppercase;
+}
+function whisper(string){
+  lowercase = string.toLowerCase();
+  string = lowercase;
+  return lowercase;
+}
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
+}
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
+}
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
+}

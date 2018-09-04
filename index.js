7032621071ddbef0df53d09e7f88922595eed595
Mielyn Acosta <mielyndl@gmail.com>
2018-09-04 20:40:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function speak(){
 
  function shout(String) {
   return String.toUpperCase()
+  return String.toLowerCase()
  }
  
  function whisper(String) {
     return String.toLowerCase()
+    return String.toUpperCase()
   }
